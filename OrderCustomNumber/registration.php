70e34c7ed4ba4d9be2a828b70fa0543c6ddb52ed
Witold Prucnel <witold.prucnel@gmail.com>
2020-12-12 11:05:41 +0100

    +added initial module files -removed readme.md

---
+++ OrderCustomNumber/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'LizardMedia_OrderCustomNumber',
+    __DIR__
+);
--- README.md
+++
@@ -1,2 +0,0 @@
-# LizardMedia_OrderCustomNumber
-Magento 2 module for order custom number feature.
